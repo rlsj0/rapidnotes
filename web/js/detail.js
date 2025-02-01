650843efdfa9bc7fb27c89d823ccce1d438342db
ceemeese <cmsalvador92@gmail.com>
2025-02-01 15:05:40 +0100

    Add link pages

---
+++ web/js/detail.js
@@ -0,0 +1,17 @@
+import User from './user.js'
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    document.getElementById("menu-toggle").addEventListener("click", function() {
+        document.getElementById("menu").classList.add("active");
+    });
+
+    document.getElementById("menu-close").addEventListener("click", function() {
+        document.getElementById("menu").classList.remove("active");
+    });
+  
+    console.log('Hola script principal')
+  
+  })
+
+  
