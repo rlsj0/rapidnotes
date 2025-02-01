44d12e1f1a65a2efc0410b4bb17c9465dbf364f3
rlsj0 <rodrileja@gmail.com>
2025-02-01 05:22:18 +0100

    add logger

--- src/Controllers/AuthController.cs
+++ src/Controllers/AuthController.cs
@@ -1,2 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController : ControllerBase
+{
+    private ILogger<AuthController> _logger;
+
+    public AuthController(ILogger<AuthController> logger)
+    {
+        _logger = logger;
+    }
+
+    /*[HttpPost("login")]*/
+    /*public IActionResult Login*/
+
+}
 
