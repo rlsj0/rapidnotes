79bcf5b89baa3afab3a5d3a04e9b80a7c05528d5
rlsj0 <rodrileja@gmail.com>
2025-02-05 18:22:10 +0100

    add modify button

--- web/js/detail.js
+++ web/js/detail.js
@@ -1,5 +1,5 @@
-import User from './user.js'
-import Note from './note.js'
+import User from "./user.js";
+import Note from "./note.js";
 const note = new Note();
 
 let noteSelected = null;
@@ -7,208 +7,213 @@ let noteSelected = null;
 //Obtener userId del sessionStorage guardado en Login
 const userId = Number(sessionStorage.getItem("userId"));
 
-
-window.addEventListener('DOMContentLoaded', (event) => {
-
-
+window.addEventListener("DOMContentLoaded", (event) => {
     //lógica para menú navegación en mobile
-    document.getElementById("menu-toggle").addEventListener("click", function() {
-        document.getElementById("menu").classList.add("active");
-    });
+    document.getElementById("menu-toggle").addEventListener(
+        "click",
+        function () {
+            document.getElementById("menu").classList.add("active");
+        },
+    );
 
-    document.getElementById("menu-close").addEventListener("click", function() {
-        document.getElementById("menu").classList.remove("active");
-    });
-    
+    document.getElementById("menu-close").addEventListener(
+        "click",
+        function () {
+            document.getElementById("menu").classList.remove("active");
+        },
+    );
 
     //Comprobación título al añadir nota en popup
-    const titleInput = document.querySelector('#title');
-    titleInput.addEventListener('input', checkTitle);
-
+    const titleInput = document.querySelector("#title");
+    titleInput.addEventListener("input", checkTitle);
 
     //Lógica para abrir y cerrar formulario popup
-    const closeCategory = document.getElementById('close-btn')
-    const addCategory = document.getElementById('add-btn')
-    const submitCategory = document.getElementById('submit-btn')
+    const closeCategory = document.getElementById("close-btn");
+    const addCategory = document.getElementById("add-btn");
+    const submitCategory = document.getElementById("submit-btn");
 
-    addCategory.addEventListener('click', () => popup.classList.add("notes__popup-container--show"))
-    closeCategory.addEventListener('click', () => popup.classList.remove("notes__popup-container--show"))
-    submitCategory. addEventListener('click', () =>  popup.classList.remove("notes__popup-container--show"))
-
+    addCategory.addEventListener(
+        "click",
+        () => popup.classList.add("notes__popup-container--show"),
+    );
+    closeCategory.addEventListener(
+        "click",
+        () => popup.classList.remove("notes__popup-container--show"),
+    );
+    submitCategory.addEventListener(
+        "click",
+        () => popup.classList.remove("notes__popup-container--show"),
+    );
 
     //Cargando notas iniciales
     console.log("Cargando notas para el usuario ID:", userId);
     note.getNotesByUserId(userId, drawNotes);
 
-
-    console.log('Hola script detalle');
-  })
-
-
-
-const deleteButton = document.querySelector("#del-btn");
-deleteButton.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  let userId = Number(sessionStorage.getItem('userId'));
-  console.log('Leyendo de nuevo el userID' + userId);
-
-
-  console.log("Esta es la nota seleccionada" + noteSelected);
-  note.deleteNote(noteSelected, () => {
-    console.log("Actualizando notas tras eliminar")
-    note.getNotesByUserId(userId, drawNotes);
-  })
-
-  clearSelection();
+    console.log("Hola script detalle");
 });
 
+const deleteButton = document.querySelector("#del-btn");
+const modifyButton = document.querySelector("#mod-btn");
 
+deleteButton.addEventListener("click", (event) => {
+    event.preventDefault();
 
+    let userId = Number(sessionStorage.getItem("userId"));
+    console.log("Leyendo de nuevo el userID" + userId);
 
-  //Pintar notas de usuario
+    console.log("Esta es la nota seleccionada" + noteSelected);
+    note.deleteNote(noteSelected, () => {
+        console.log("Actualizando notas tras eliminar");
+        note.getNotesByUserId(userId, drawNotes);
+    });
+
+    clearSelection();
+});
+
+modifyButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let userId = Number(sessionStorage.getItem("userId"));
+    console.log("Leyendo de nuevo el userID" + userId);
+
+    console.log("Esta es la nota seleccionada" + noteSelected);
+    //:
+    //note.modifyNote();
+    //note.getNotesByUserId(userId, drawNotes);
+    //});
+
+    clearSelection();
+});
+
+//Pintar notas de usuario
 function drawNotes(data) {
-
     const notesContainer = document.getElementById("notes-container");
     notesContainer.innerHTML = "";
 
-    console.log("Length de data" + data.length)
+    console.log("Length de data" + data.length);
 
     //Comprobación notas -> mostrar no hay notas
     if (!data || data.length === 0) {
-        let titleInformationNotes = document.createElement('h3');
-        titleInformationNotes.innerText = 'No tienes ninguna nota';
+        let titleInformationNotes = document.createElement("h3");
+        titleInformationNotes.innerText = "No tienes ninguna nota";
         titleInformationNotes.classList.add("notes__container-title");
         notesContainer.appendChild(titleInformationNotes);
         return;
     }
 
     //Se crea lista de notas
-    const noteUl = document.createElement('ul');
-    noteUl.classList.add('notes__list');
+    const noteUl = document.createElement("ul");
+    noteUl.classList.add("notes__list");
     notesContainer.appendChild(noteUl);
 
     //Se crean los elementos
-    data.forEach(note => {
-        
+    data.forEach((note) => {
         //Cada nota
-        const noteElement = document.createElement('li');
-        noteElement.setAttribute('data-id', note.id)
-        noteElement.classList.add('notes__list-element');
+        const noteElement = document.createElement("li");
+        noteElement.setAttribute("data-id", note.id);
+        noteElement.classList.add("notes__list-element");
 
         //Cada link de nota
-        const noteLink =  document.createElement('a');
-        noteLink.href = 'detail.html#/'+ userId;
-        noteLink.classList.add('notes__list-link')
+        const noteLink = document.createElement("a");
+        noteLink.href = "detail.html#/" + userId;
+        noteLink.classList.add("notes__list-link");
 
         //Cada titulo nota
-        const noteTitle = document.createElement('h3')
-        noteTitle.classList.add('notes__list-title');
+        const noteTitle = document.createElement("h3");
+        noteTitle.classList.add("notes__list-title");
         noteTitle.innerText = note.title;
 
         //Cada texto de nota
-        const noteText = document.createElement('p');
-        noteText.classList.add('notes__list-text');
+        const noteText = document.createElement("p");
+        noteText.classList.add("notes__list-text");
         noteText.innerText = note.text;
 
         noteUl.appendChild(noteElement);
         noteElement.appendChild(noteLink);
         noteLink.appendChild(noteTitle);
         noteLink.appendChild(noteText);
-
     });
 
     //Seleccionamos la Lista de notas y el boton delete
-    const listItems = document.querySelectorAll('.notes__list-element');
-    const deleteButton = document.getElementById('del-btn');
+    const listItems = document.querySelectorAll(".notes__list-element");
+    const deleteButton = document.getElementById("del-btn");
 
-    listItems.forEach(note => {
+    listItems.forEach((note) => {
+        note.addEventListener("click", function (event) {
+            if (this.classList.contains("notes__list-element--active")) {
+                // Quitamos la clase activa y reseteamos el selectedId
+                this.classList.remove("notes__list-element--active");
+                this.querySelector("a").blur();
 
-        note.addEventListener('click', function(event) {
-    
-            if (this.classList.contains('notes__list-element--active')) {
-              // Quitamos la clase activa y reseteamos el selectedId
-              this.classList.remove('notes__list-element--active');
-              this.querySelector('a').blur();
-              
-              clearSelection();
-              
+                clearSelection();
             } else {
-              
-              noteSelected = this.getAttribute('data-id');
-              console.log("Esta es la nota seleccionada: " + noteSelected);
-              document.querySelectorAll('.notes__list-element').forEach(element => {
-                element.classList.remove('notes__list-element--active');
-              });
-        
-              this.classList.add('notes__list-element--active');
-              deleteButton.disabled = false;
-    
-              // hash de ruta modificado
-              //window.location.hash = `/${selectedId}`
+                noteSelected = this.getAttribute("data-id");
+                console.log("Esta es la nota seleccionada: " + noteSelected);
+                document.querySelectorAll(".notes__list-element").forEach(
+                    (element) => {
+                        element.classList.remove("notes__list-element--active");
+                    },
+                );
+
+                this.classList.add("notes__list-element--active");
+                deleteButton.disabled = false;
+                modifyButton.disabled = false;
+
+                // hash de ruta modificado
+                //window.location.hash = `/${selectedId}`
             }
         });
-        
     });
 }
 
-
-
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
     const clickedInsideNote = event.target.closest(".notes__list-element");
 
-    if (!clickedInsideNote) { 
-        
+    if (!clickedInsideNote) {
         console.log("🔹 Clic fuera de la lista, limpiando selección...");
         clearSelection();
     }
 });
 
-
-
-
 //Check titulo al añadir nueva nota
 function checkTitle() {
+    const noteInput = document.querySelector("#title");
+    const submitButton = document.querySelector("#submit-btn");
 
-    const noteInput = document.querySelector('#title');
-    const submitButton = document.querySelector('#submit-btn');
-  
-    if(noteInput.value === '' ) {
-      submitButton.disabled = true
+    if (noteInput.value === "") {
+        submitButton.disabled = true;
     } else {
-      submitButton.disabled = false
+        submitButton.disabled = false;
     }
-  }
+}
 
-
-  function clearSelection() {
+function clearSelection() {
     noteSelected = null;
     deleteButton.disabled = true;
-    console.log('Selección limpia, botón deshabilitado')
-  
+    modifyButton.disabled = true;
+    console.log("Selección limpia, botón deshabilitado");
+
     //se limpia hash si no hay categoria seleccionada
     //window.location.hash = '';
-  }
+}
 
-
-  //Evento de envío de formulario
-formNote.addEventListener('submit', async function (event) {
+//Evento de envío de formulario
+formNote.addEventListener("submit", async function (event) {
     event.preventDefault();
 
-    const userId = Number(sessionStorage.getItem('userId'));
-    const noteTitle = document.querySelector('#title').value;
-    const noteDescription = document.querySelector('#description').value;
-    const notePriority = document.querySelector('#priority').value;
+    const userId = Number(sessionStorage.getItem("userId"));
+    const noteTitle = document.querySelector("#title").value;
+    const noteDescription = document.querySelector("#description").value;
+    const notePriority = document.querySelector("#priority").value;
 
-    console.log('Añadiendo nueva nota');
+    console.log("Añadiendo nueva nota");
 
     //Tras añadir nota, refrescar lista (callback)
-    note.addNote(userId,noteTitle, noteDescription, notePriority, () => {
-        console.log("Actualizando notas tras añadir")
+    note.addNote(userId, noteTitle, noteDescription, notePriority, () => {
+        console.log("Actualizando notas tras añadir");
         note.getNotesByUserId(userId, drawNotes);
     });
-   
+
     //reseteo de formulario
     formNote.reset();
-})
-
+});
